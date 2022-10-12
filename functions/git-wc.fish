de7e2222b50e140d011ac12b4e45fb43993944d3
Shawn Sorichetti <ssoriche@users.noreply.github.com>
2022-10-12 10:19:47 -0400

    feat: handle case when branch is not specified
    No longer require a branch parameter as the source point, default to
    origin/main.

--- functions/git-wc.fish
+++ functions/git-wc.fish
@@ -1,4 +1,5 @@
 function git-wc -d "create a new worktree and branch" --wraps git-worktree
-  git worktree add {-b,}$argv[1] $argv[2..-1]
+  set -q branch $argv[2] || set branch origin/main
+  git worktree add {-b,}$argv[1] $branch $argv[3..-1]
   cd $argv[1]
 end
