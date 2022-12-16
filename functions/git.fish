a9776c3342ffea25bc2b25484628942e12783d1c
Shawn Sorichetti <ssoriche@users.noreply.github.com>
2022-12-16 14:49:46 -0500

    refactor: use fish abbreviation for g
    This is the introduction of using abbreviations instead of a function
    for git. Abbreviations are nice as they automatically expand out to the
    command that they are replacing.
    
    Using an abbreviation required changing the git function so that it
    called the actual command, to not create an infinite loop.

--- functions/git.fish
+++ functions/git.fish
@@ -1,8 +1,9 @@
-function g -d "short cut for accessing git" --wraps git
+function git -d "function to access fish git functions" --wraps git
   set -l git_func "git-$argv[1]"
+  set -l git_cmd (command -v git)
   if functions -d -- $git_func &> /dev/null
     $git_func $argv[2..-1] 
   else
-    git $argv
+    $git_cmd $argv
   end
 end
