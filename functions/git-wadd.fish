6afdd1cfea6479134ffb88f18ce2df79df8c3f5a
Shawn Sorichetti <ssoriche@users.noreply.github.com>
2024-01-02 16:01:11 -0500

    style(wadd): apply styling

--- functions/git-wadd.fish
+++ functions/git-wadd.fish
@@ -1,5 +1,9 @@
 function git-wadd -d "create a new worktree and branch" --wraps git-worktree
-  set -q branch $argv[2] || set branch origin/main
-  git worktree add {-b,}$argv[1] $branch $argv[3..-1]
-  cd $argv[1]
+    set -l branch $argv[2]
+    if test (count $argv) -lt 2
+        set branch origin/main
+    end
+
+    git worktree add {-b ,}$argv[1] $branch $argv[3..-1]
+    cd $argv[1]
 end
